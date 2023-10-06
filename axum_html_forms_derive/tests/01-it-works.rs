c3d3aaef50706d3ee6ae7075b404e837ac823cf5
James Rampton <me@jamesrampton.com>
2023-10-06 10:25:48 +0100

    Parse `Option<String>`

--- axum_html_forms_derive/tests/01-it-works.rs
+++ axum_html_forms_derive/tests/01-it-works.rs
@@ -5,6 +5,7 @@ mod inner {
     pub struct SimpleForm {
         pub name: String,
         pub age: u8,
+        pub favourite_word: Option<String>,
     }
 
     use axum_html_forms_derive::HtmlForm;
@@ -14,11 +15,18 @@ fn main() {
     let original = SimpleForm {
         name: String::from("John"),
         age: 69,
+        favourite_word: Some(String::from("Buns")),
     };
 
-    let unchecked = inner::SimpleFormUnchecked {
+    let unchecked_1 = inner::SimpleFormUnchecked {
         name: Some(String::from("Jane")),
         age: Some(String::from("42")),
+        favourite_word: None,
+    };
+    let unchecked_2 = inner::SimpleFormUnchecked {
+        name: Some(String::from("Alice")),
+        age: Some(String::from("69")),
+        favourite_word: Some(String::from("Beans")),
     };
 
     let form = inner::SimpleFormHtmlForm::default();
@@ -27,11 +35,12 @@ fn main() {
     let html = format!("{nodes:#}");
     eprintln!("{}", html);
 
-    let parsed_form = SimpleForm::try_from(&unchecked);
+    let parsed_form = SimpleForm::try_from(&unchecked_1);
 
     let _ = original.name;
     let _ = original.age;
-    let _ = unchecked;
+    let _ = unchecked_1;
+    let _ = unchecked_2;
     let _ = form;
     let _ = parsed_form;
 }
--- main.rs
+++ main.rs
@@ -2,17 +2,20 @@
 pub struct SimpleForm {
     pub name: String,
     pub age: u8,
+    pub favourite_word: Option<String>,
 }
 
 fn main() {
     let original = SimpleForm {
         name: String::from("John"),
         age: 69,
+        favourite_word: Some(String::from("Buns")),
     };
 
     let unchecked = SimpleFormUnchecked {
         name: Some(String::from("Jane")),
         age: Some(String::from("42")),
+        favourite_word: None,
     };
 
     // let form_input = SimpleFormFields {
